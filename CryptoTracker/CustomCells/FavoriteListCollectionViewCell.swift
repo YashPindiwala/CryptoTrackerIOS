68cc74fb75617e61e5fd31be96b6c22fa4bd854f
YashPindiwala <w0790912@myscc.ca>
2023-10-21 19:34:39 -0400

    added necessary viewcontrollers

---
+++ CryptoTracker/CustomCells/FavoriteListCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteListCollectionViewCell.swift
+//  CryptoTracker
+//
+//  Created by yash pindiwala on 2023-10-21.
+//
+
+import UIKit
+
+class FavoriteListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var coinImageView: UIImageView!
+    @IBOutlet var coinSymbolLabel: UILabel!
+}
